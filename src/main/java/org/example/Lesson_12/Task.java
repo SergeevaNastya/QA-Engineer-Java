7f3dcf483f4996e2932257a69fc5a69156913db5
Sergeeva <elka0953@gmail.com>
2024-08-20 11:41:09 +0300

    Add classes for Lesson_12

---
+++ src/main/java/org/example/Lesson_12/Task.java
@@ -0,0 +1,33 @@
+package org.example.Lesson_12;
+
+public class Task {
+    public static void checkSize(String [][] array) throws MyArraySizeException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Ожидался массив размером 4x4");
+        }
+    }
+
+    public static int sumArray(String[][] array) throws MyArrayDataException {
+        int sum = 0;
+        for (String[] strings : array) {
+            for (String string : strings) {
+                try {
+                    sum += Integer.parseInt(string);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Преобразование не удалось");
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void validateArray(String[][] array) {
+        try {
+            checkSize(array);
+            int sum = sumArray(array);
+            System.out.println("Массив корректного размера. Сумма элементов: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,23 @@
 package org.example;
 
-import org.example.Lesson_10_Task1.Animal;
-import org.example.Lesson_10_Task1.Cat;
-import org.example.Lesson_10_Task1.Dog;
-import org.example.Lesson_10_Task1.FoodBowl;
-import org.example.Lesson_10_Task2.Circle;
-import org.example.Lesson_10_Task2.Rectangle;
-import org.example.Lesson_10_Task2.Triangle;
+import org.example.Lesson_12.*;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] correctArray = {
+                {"5", "7", "1", "0"},
+                {"5", "2", "9", "10"},
+                {"11", "0", "-8", "5"},
+                {"1", "3", "8", "8"}
+        };
+
+        String[][] incorrectArray = {
+                {"1", "1"},
+                {"1", "1"},
+                {"1", "1"}
+        };
+
+        Task.validateArray(correctArray);
+        Task.validateArray(incorrectArray);
     }
 }
