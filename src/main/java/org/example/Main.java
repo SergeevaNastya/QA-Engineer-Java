8bc28ffd8b1772175543b0cfaf74a4147be256b5
Sergeeva <elka0953@gmail.com>
2024-08-20 10:37:32 +0300

    clean class main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,36 +10,5 @@ import org.example.Lesson_10_Task2.Triangle;
 
 public class Main {
     public static void main(String[] args) {
-        Cat[] cats = {
-                new Cat("Барсик"),
-                new Cat("Багет"),
-                new Cat("Мурзик"),
-                new Cat("Василий"),
-                new Cat("Снежок"),
-                new Cat("Персик")
-        };
-
-        System.out.println("Количество созданных котов: " + Cat.catCount);
-        System.out.println("Количество созданных собак: " + Dog.dogCount);
-        System.out.println("Количество созданных животных: " + Animal.animalCount);
-
-        FoodBowl foodBowl = new FoodBowl(50);
-        foodBowl.addFood(100);
-
-        for (Cat cat : cats) {
-            foodBowl.reduceFood(Cat.MIN_FOOD);
-            cat.eat(Cat.MIN_FOOD);
-            System.out.println(cat.getName() + " сытость: " + cat.isFull());
-        }
-
-        System.out.println("Оставшееся количество еды в миске: " + foodBowl.getFoodCount());
-
-        Triangle triangle = new Triangle("красный", "черный", 10, 15, 20);
-        Rectangle rectangle = new Rectangle("желтый", "черный", 20, 5);
-        Circle circle = new Circle("синий", "фиолетовый", 25);
-
-        triangle.printCalculation();
-        rectangle.printCalculation();
-        circle.printCalculation();
     }
 }
