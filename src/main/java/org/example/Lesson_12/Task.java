e07aa97682c4ec1e46392fdff28a1937f266d1d2
Sergeeva <elka0953@gmail.com>
2024-08-21 09:55:08 +0300

    Update Lesson_12

--- src/main/java/org/example/Lesson_12/Task.java
+++ src/main/java/org/example/Lesson_12/Task.java
@@ -1,7 +1,7 @@
 package org.example.Lesson_12;
 
 public class Task {
-    public static void checkSize(String [][] array) throws MyArraySizeException {
+    public static void checkSize(String[][] array) throws MyArraySizeException {
         if (array.length != 4 || array[0].length != 4) {
             throw new MyArraySizeException("Ожидался массив размером 4x4");
         }
@@ -14,7 +14,7 @@ public class Task {
                 try {
                     sum += Integer.parseInt(string);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Преобразование не удалось");
+                    throw new MyArrayDataException("Преобразование не удалось. Некорректный символ: " + string);
                 }
             }
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,10 @@ import org.example.Lesson_12.*;
 public class Main {
     public static void main(String[] args) {
         String[][] correctArray = {
-                {"5", "7", "1", "0"},
+                {"5", "*", "1", "0"},
                 {"5", "2", "9", "10"},
                 {"11", "0", "-8", "5"},
-                {"1", "3", "8", "8"}
+                {"1", "5", "8", "8"}
         };
 
         String[][] incorrectArray = {
