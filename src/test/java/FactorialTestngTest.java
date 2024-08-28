e6d2b3bf8466d399a72c0205d1799a23057ad2e1
Sergeeva <elka0953@gmail.com>
2024-08-28 16:57:43 +0300

    Update class

--- src/test/java/FactorialTestngTest.java
+++ src/test/java/FactorialTestngTest.java
@@ -1,18 +1,25 @@
 import org.example.Lesson_14_testng.FactorialTestng;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class FactorialTestngTest {
+
+    private static FactorialTestng factorial;
+
+    @BeforeClass
+    public void testSetUp() {
+        factorial = new FactorialTestng();
+    }
+
     @Test
-    public void testFactorialTestng_1() {
-        FactorialTestng factorial = new FactorialTestng();
+    public void testFactorialTestngOne() {
         int result = factorial.getFactorial(5);
         Assert.assertEquals(result, 120);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testFactorialTestng_2() {
-        FactorialTestng factorial = new FactorialTestng();
+    public void testFactorialTestngTwo() {
         factorial.getFactorial(-5);
     }
 }
