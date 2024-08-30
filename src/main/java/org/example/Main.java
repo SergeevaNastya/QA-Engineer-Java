ddf28c3d13f545b918f66ce44c1836f5f660d5a4
Sergeeva <elka0953@gmail.com>
2024-08-30 14:42:31 +0300

    Done Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/elka0/IdeaProjects/QA-Java/src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.mts.by/");
+
     }
 }
